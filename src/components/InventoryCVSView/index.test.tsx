6392e359c0e9a45c1aafd9cc17ac1cc657c06c48
Alon <od120162@gmail.com>
2017-11-18 09:13:34 +0200

    ..

--- src/components/InventoryCVSView/index.test.tsx
+++ src/components/InventoryCVSView/index.test.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as TypeDefaults from '../../types/TypeDefaults'
-import InventoryRFUView from './';
-import {InventoryRFUViewProps } from './';
+import InventoryCVSView from './';
+import {InventoryCVSViewProps } from './';
 import * as Theme from './Theme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  const props: InventoryRFUViewProps = {
-    inventoryRFUData: TypeDefaults.INVENTORY_DEVICES_DEFAULT,
+  const props: InventoryCVSViewProps = {
+    inventoryCVSData: TypeDefaults.INVENTORY_DEVICES_DEFAULT,
     theme: Theme.DEFAULT_THEME,
   }
 
-  ReactDOM.render(<MuiThemeProvider><InventoryRFUView {...props}/></MuiThemeProvider>, div);
+  ReactDOM.render(<MuiThemeProvider><InventoryCVSView {...props}/></MuiThemeProvider>, div);
 });
